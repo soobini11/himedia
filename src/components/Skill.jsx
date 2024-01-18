f149f1fdade2b83be74bcda18a9c7849b44eae2b
soobini11 <156270677+soobini11@users.noreply.github.com>
2024-01-18 11:44:11 +0900

    8번 스킬 완료

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
+const skillText =[
+  {
+    title : "우리의 목표는 100% 취업이다.",
+    desc : "포트폴리오가 완성이 되어야합니다. 포트폴리오는 본인의 작품을 보기 좋게 디자인하여 보는 사람이 편하게 본인의 실력을 볼 수 있도록 하는 것이 가장 중요합니다. 개성이 강한 것도 좋지만 보는 사람들이 편한 작품이 지금은 더 중요한 듯 합니다.",
+  },
+  {
+    title : "나의 스펙 쌓기",
+    desc : "포트폴리오가 완성이 되어야합니다. 포트폴리오는 본인의 작품을 보기 좋게 디자인하여 보는 사람이 편하게 본인의 실력을 볼 수 있도록 하는 것이 가장 중요합니다. 개성이 강한 것도 좋지만 보는 사람들이 편한 작품이 지금은 더 중요한 듯 합니다.",
+  },
+  {
+    title : "우리의 목표는 100% 취업이다.",
+    desc : "포트폴리오가 완성이 되어야합니다. 포트폴리오는 본인의 작품을 보기 좋게 디자인하여 보는 사람이 편하게 본인의 실력을 볼 수 있도록 하는 것이 가장 중요합니다. 개성이 강한 것도 좋지만 보는 사람들이 편한 작품이 지금은 더 중요한 듯 합니다.",
+  },
+]
+
 const Skill = () => {
   return (
-    <div>
-      Skill
-    </div>
+    <section id="skill">
+      <div className="skill_inner">
+        <h2 className='skill_title'>Challenge <em>나의 도전</em></h2>
+        <div className="skill_desc">
+          {skillText.map((skill,key)=>(
+            <div key={key}>
+            <span>{key+1}</span>
+            <h3>{Skill.title}</h3>
+            <p>{skill.desc}</p>
+          </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
